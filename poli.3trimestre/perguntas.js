5fbfc0e8c6a3f2489b9c951ef708c71cdb94a095
whoshya <135127340+whoshya@users.noreply.github.com>
2024-11-04 08:30:25 -0300

    Add files via upload

---
+++ poli.3trimestre/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Anatomia',
+    'Quantos ossos temos no nosso corpo?',
+    '206. O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores.'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou Mona Lisa?',
+    'Leonardo da Vinci. Mona Lisa, uma das pinturas mais famosas do mundo, foi pintada entre os anos 1503 e 1506 por Leonardo da Vinci (1452-1519). A obra está exposta no Museu do Louvre, na França.'
+)
+
+criaCartao(
+    'Português',
+    'Qual o plural de chapéu?',
+    'Chapéus. Os substantivos terminado em ditongos orais - como chapéu - fazem plural acrescentando -s no final.'
+)
+
+criaCartao(
+    'Geografia',
+    'Que país tem o formato de uma bota?',
+    'Itália. No mapa da Europa, a Itália se destaca por parecer uma bota de cano e salto altos.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin. O russo Yuri Gagarin (1934-1968) foi a primeira pessoa a viajar para o espaço, o que aconteceu em 12 de abril de 1961.'
+)
